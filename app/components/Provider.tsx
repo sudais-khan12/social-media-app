863153616553e299d7cbc70d28ac3d510536cbd0
sudais-khan12 <155130846+sudais-khan12@users.noreply.github.com>
2025-03-07 17:52:37 +0500

    version updated

--- app/components/Provider.tsx
+++ app/components/Provider.tsx
@@ -1,38 +1,40 @@
 import React from "react";
 import { ImageKitProvider } from "imagekitio-next";
+import { SessionProvider } from "next-auth/react";
 
 const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
 const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY;
-const authenticator = async () => {
-  try {
-    const response = await fetch("/api/imagrkitAuth");
 
-    if (!response.ok) {
-      const errorText = await response.text();
-      throw new Error(
-        `Request failed with status ${response.status}: ${errorText}`
-      );
+export default function Provider({ children }: { children: React.ReactNode }) {
+  const authenticator = async () => {
+    try {
+      const response = await fetch("/api/imagrkitAuth");
+
+      if (!response.ok) {
+        const errorText = await response.text();
+        throw new Error(
+          `Request failed with status ${response.status}: ${errorText}`
+        );
+      }
+
+      const data = await response.json();
+      const { signature, expire, token } = data;
+      return { signature, expire, token };
+    } catch (error) {
+      console.error(error);
+      throw new Error(`Authentication request failed`);
     }
+  };
 
-    const data = await response.json();
-    const { signature, expire, token } = data;
-    return { signature, expire, token };
-  } catch (error) {
-    throw new Error(`Authentication request failed: ${error}`);
-  }
-};
-
-export default function Home() {
   return (
-    <div className="App">
+    <SessionProvider>
       <ImageKitProvider
         urlEndpoint={urlEndpoint}
         publicKey={publicKey}
         authenticator={authenticator}
       >
-        {/* ...client side upload component goes here */}
+        {children}
       </ImageKitProvider>
-      {/* ...other SDK components added previously */}
-    </div>
+    </SessionProvider>
   );
 }
